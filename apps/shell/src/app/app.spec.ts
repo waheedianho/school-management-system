64d34d35bb6e09300db1605e32a0f96311843329
Waheed Safiu <waheedianho65@gmail.com>
2026-06-23 11:24:45 +0100

    feat(config): adjust config-test

--- apps/shell/src/app/app.spec.ts
+++ apps/shell/src/app/app.spec.ts
@@ -1,20 +1,48 @@
 import { TestBed } from '@angular/core/testing';
-import { App } from './app';
-import { NxWelcome } from './nx-welcome';
+import { provideRouter } from '@angular/router';
+import { RouterTestingHarness } from '@angular/router/testing';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { appRoutes } from './app.routes';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App, NxWelcome],
+      providers: [provideRouter(appRoutes), provideAnimationsAsync()],
     }).compileComponents();
   });
 
-  // it('should render title', async () => {
-  //   const fixture = TestBed.createComponent(App);
-  //   await fixture.whenStable();
-  //   const compiled = fixture.nativeElement as HTMLElement;
-  //   expect(compiled.querySelector('h1')?.textContent).toContain(
-  //     'Welcome shell',
-  //   );
-  // });
+  it('should render page for parent when url is /parent', async () => {
+    const harness = await RouterTestingHarness.create('/parent');
+    expect(
+      harness.routeNativeElement?.querySelector('p')?.textContent,
+    ).toContain('Parent works!');
+  });
+
+  it('should render page for teacher when url is /teacher', async () => {
+    const harness = await RouterTestingHarness.create('/teacher');
+    expect(
+      harness.routeNativeElement?.querySelector('p')?.textContent,
+    ).toContain('Teacher works!');
+  });
+
+  it('should render page for student when url is /student', async () => {
+    const harness = await RouterTestingHarness.create('/student');
+    expect(
+      harness.routeNativeElement?.querySelector('p')?.textContent,
+    ).toContain('Student works!');
+  });
+
+  it('should render page for school admin when url is /school-admin', async () => {
+    const harness = await RouterTestingHarness.create('/school-admin');
+    expect(
+      harness.routeNativeElement?.querySelector('p')?.textContent,
+    ).toContain('SchoolAdmin works!');
+  });
+
+  it('should render page for super admin when url is /super-admin', async () => {
+    const harness = await RouterTestingHarness.create('/super-admin');
+    expect(
+      harness.routeNativeElement?.querySelector('p')?.textContent,
+    ).toContain('SuperAdmin works!');
+  });
 });
